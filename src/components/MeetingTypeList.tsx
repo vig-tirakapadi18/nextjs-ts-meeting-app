8200395f64cb46d7a021c9dfae16a6c802194f4d
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2024-12-21 11:59:59 +0530

    completed home page design

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -5,6 +5,7 @@ import HomeCard from "./HomeCard";
 import { FaCalendarAlt } from "react-icons/fa";
 import { HiUserAdd, HiVideoCamera } from "react-icons/hi";
 import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -12,6 +13,9 @@ const MeetingTypeList = () => {
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
 
+  const createMeeting = () => {
+    
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -50,6 +54,15 @@ const MeetingTypeList = () => {
       >
         <HiUserAdd size={40} />
       </HomeCard>
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
     </section>
   );
 };
