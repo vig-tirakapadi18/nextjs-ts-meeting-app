42d8b2ad955fcda37cb3c337a23f9624c61a745a
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2024-12-23 13:03:33 +0530

    next.config.ts to next.config.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Voom",
@@ -43,9 +32,7 @@ export default function RootLayout({
           },
         }}
       >
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-2`}
-        >
+        <body className={`antialiased bg-dark-2`}>
           {children}
           <Toaster />
         </body>
